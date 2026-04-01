38a714750b783eae1b99931ed9f99757acb08cba
Bradley D Biehl <bradley.biehl@gmail.com>
2026-04-01 16:44:30 -0500

    [layout] feat: remove unused provideAnimationsAsync from app configuration

--- projects/public-app/src/app/app.config.ts
+++ projects/public-app/src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
@@ -10,6 +9,5 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideAnimationsAsync(),
   ],
 };
