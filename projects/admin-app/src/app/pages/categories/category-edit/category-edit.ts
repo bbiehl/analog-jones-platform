d599b7430ee7be02c9f3c03c0e5330969fcde185
Bradley D Biehl <bradley.biehl@gmail.com>
2026-04-07 08:34:35 -0500

    [brad-scaffold] feat: add category add and edit components with routing

---
+++ projects/admin-app/src/app/pages/categories/category-edit/category-edit.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-edit',
+  imports: [],
+  templateUrl: './category-edit.html',
+  styleUrl: './category-edit.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CategoryEdit {}
