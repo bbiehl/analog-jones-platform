4666b83e372a899a67481230a98a8a9a04b2da93
Bradley D Biehl <bradley.biehl@gmail.com>
2026-04-07 08:38:09 -0500

    [brad-scaffold] feat: add genre add and edit components with routing

---
+++ projects/admin-app/src/app/pages/genres/genre-edit/genre-edit.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-genre-edit',
+  imports: [],
+  templateUrl: './genre-edit.html',
+  styleUrl: './genre-edit.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GenreEdit {}
