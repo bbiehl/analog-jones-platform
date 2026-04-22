c4af78ee1d59cf81721420fe7f5641371a7d77a0
Bradley D Biehl <bradley.biehl@gmail.com>
2026-04-22 17:37:27 -0500

    [explore-feature] refactor: update Explorer page content and remove unused imports

--- projects/public-app/src/app/pages/explorer/explorer.ts
+++ projects/public-app/src/app/pages/explorer/explorer.ts
@@ -1,11 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-explorer',
-  imports: [RouterLink, RouterOutlet, MatCardModule, MatIconModule],
+  imports: [],
   templateUrl: './explorer.html',
   styleUrl: './explorer.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
